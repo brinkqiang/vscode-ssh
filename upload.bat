6cbb03f3617873a00d44de01c9a5e47629f893c8
brinkqiang <brink.qiang@gmail.com>
2020-01-14 12:50:57 +0800

    add

--- upload.bat
+++ upload.bat
@@ -2,4 +2,6 @@
 set username=%1%
 set ip=%2%
 
-scp .vscode-server-insiders.zip %username%@%ip%:/home/%username%
+scp -P 22 ./setup/.vscode-server-insiders.zip %username%@%ip%:/home/%username%
+
+ssh -p 22 %username%@%ip% "unzip -o -d ~/ ~/.vscode-server-insiders.zip"
--- upload.sh
+++ upload.sh
@@ -3,4 +3,6 @@
 username=$1
 ip=$2
 
-scp .vscode-server-insiders.zip $username@$ip:/home/$username
+scp -P 22 ./setup/.vscode-server-insiders.zip $username@$ip:/home/$username
+
+ssh -p 22 $username@$ip "unzip -o -d ~/ ~/.vscode-server-insiders.zip"
