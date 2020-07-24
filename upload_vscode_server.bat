a1ddfae1099126970acfcd87335157f77ee354fe
brinkqiang <brink.qiang@gmail.com>
2020-07-24 16:33:55 +0800

    add

--- upload_vscode_server.bat
+++ upload_vscode_server.bat
@@ -21,4 +21,8 @@ pscp -P %port% -pw %passwd% setup\.vscode-server.zip %username%@%ip%:~
 
 plink -P %port% -pw %passwd% %username%@%ip% "unzip -o -d ~/ ~/.vscode-server.zip" 
 
+plink -P %port% -pw %passwd% %username%@%ip% "chmod +x ~/.vscode-server/bin/cd9ea6488829f560dc949a8b2fb789f3cdc05f5d/node"
+
+plink -P %port% -pw %passwd% %username%@%ip% "chmod +x ~/.vscode-server/bin/cd9ea6488829f560dc949a8b2fb789f3cdc05f5d/server.sh"
+
 echo upload .vscode-server end
