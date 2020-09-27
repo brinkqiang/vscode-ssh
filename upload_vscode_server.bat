c7852aec87fd6b43f05ed2d779d9b3db82acb835
brinkqiang <brink.qiang@gmail.com>
2020-09-27 11:19:22 +0800

    add

--- upload_vscode_server.bat
+++ upload_vscode_server.bat
@@ -17,7 +17,7 @@ echo username="%username%" ip="%ip%" port="%port%"
 
 echo upload .vscode-server start
 
-pscp -P %port% -pw %passwd% .\setup\.vscode-server.zip %username%@%ip%:
+pscp -P %port% -pw %passwd% %~dp0setup\.vscode-server.zip %username%@%ip%:
 
 plink -P %port% -pw %passwd% %username%@%ip% "unzip -o -d ~/ ~/.vscode-server.zip" 
 
