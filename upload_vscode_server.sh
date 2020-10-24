149aa5b5554e369e001231a2c946bee0c97fec70
brinkqiang <brink.qiang@gmail.com>
2020-10-24 16:22:01 +0800

    add

--- upload_vscode_server.sh
+++ upload_vscode_server.sh
@@ -38,12 +38,20 @@ fi
 
 echo username="$username" ip="$ip" port="$port"
 
+NODE_PATH = $(shell $(if $(wildcard  ~/.vscode-server/bin),find  ~/.vscode-server/bin -maxdepth 3 -name "node",))
+SERVER_PATH = $(shell $(if $(wildcard  ~/.vscode-server/bin),find  ~/.vscode-server/bin -maxdepth 3 -name "server.sh",))
+
+define SSHPASS_EXEC
+    sshpass -p "$passwd" ssh -p $port $username@$ip "chmod +x $(1)"
+endef
+
 echo upload .vscode-server start
 
 sshpass -p "$passwd" scp -P $port ./setup/.vscode-server.zip $username@$ip:~/.vscode-server.zip
 
 sshpass -p "$passwd" ssh -p $port $username@$ip "unzip -o -d ~/ ~/.vscode-server.zip"
-sshpass -p "$passwd" ssh -p $port $username@$ip "chmod +x ~/.vscode-server/bin/cd9ea6488829f560dc949a8b2fb789f3cdc05f5d/node"
-sshpass -p "$passwd" ssh -p $port $username@$ip "chmod +x ~/.vscode-server/bin/cd9ea6488829f560dc949a8b2fb789f3cdc05f5d/server.sh"
+
+$(foreach NODE_DIR, $(NODE_PATH), $(eval $(call SSHPASS_EXEC,$(NODE_DIR))))
+$(foreach SERVER_DIR, $(SERVER_PATH), $(eval $(call SSHPASS_EXEC,$(NODE_DIR))))
 
 echo upload .vscode-server end
